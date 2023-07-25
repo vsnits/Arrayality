2dcae3b5c513cdd29da69812f96d107ac2104347
Vsevolod Nitsenko <130823130+vsnits@users.noreply.github.com>
2023-07-25 20:48:39 +0300

    Add files via upload

---
+++ Arrayality.js
@@ -0,0 +1,138 @@
+
+  let board, origin, mx = 4, width = 16, moves = 0
+  let clwidth = Math.ceil(innerWidth*0.8 / (width))
+  
+  /* core */
+  function score(b) {
+      var s = 1
+      for(var i = 1; i <= mx; i++) {
+          for(var p = 0; p < b.length; p++) {
+              var c = 0
+              for(var e = p; e < b.length; e = e + i) {
+                  if(b[e] == i.toString() && !b[e].mark && b[e].mark != i) {
+                      b[e].mark = i
+                      c++
+                      } else { break }
+                  }
+              c? (s *= c) : 0
+              }
+          }
+      return s
+      };
+
+  function clear() {
+      for(var i = 0; i < board.length; i++) {
+          board[i].mark = null
+          }
+      };
+
+  function swap(p1, p2) {
+      moves++
+      var i1 = board[p1]
+      var i2 = board[p2]
+      board.splice(p1, 1, i2)
+      board.splice(p2, 1, i1)
+      makeboard()
+      };
+
+  function entergame() {
+      board = Array.from({ length: width }, () => new String(Math.floor(Math.random() * mx)+1))
+      while(score(board) > mx) {
+          board =  Array.from({ length: width }, () => new String(Math.floor(Math.random() * mx)+1))
+          }
+      origin = Array.from(board)
+      makeboard()
+      };
+
+  function playback() {
+      moves = 0
+      board = Array.from(origin)
+      makeboard()
+      };
+
+  /* graphics */
+  var canv = document.getElementById("canv")
+  canv.height = clwidth
+  canv.width = width*clwidth
+  var ctx = canv.getContext("2d")
+  ctx.font = `${clwidth*0.8}px serif`
+  
+  function makeboard() {
+      clear()
+      var sc = score(board)
+      draw(sc)
+      document.getElementById("score").innerHTML = `Score ${sc}`
+      document.getElementById("moves").innerHTML = `Moves ${moves}`  
+      };
+
+  canv.style.border = "1px solid"
+  function draw(s) {
+      ctx.clearRect(0,0,canv.width, canv.height)
+      for(var i = clwidth; i < canv.width; i += clwidth) {
+          ctx.beginPath()
+          ctx.fillStyle = "black"
+          ctx.fillRect(i, 0, 1, clwidth)
+          ctx.closePath()
+          }
+      for(var i = 0; i < board.length; i++) {
+          // switch case glitches!
+          if(board[i] == "1") { dr("blue", i); }
+          if(board[i] == "2") { dr("orange", i); }   
+          if(board[i] == "3") { dr("green", i); }
+          if(board[i] == "4") { dr("red", i); } 
+          if(board[i] == "5") { dr("purple", i); }  
+          }
+      };
+
+  function dr(color, i) { 
+      ctx.fillStyle = color
+      ctx.fillText(board[i].toString(), (i+0.3)*clwidth, clwidth*0.8)
+      };
+
+  /* events */
+  
+  function X(e) {
+      return e.pageX-canv.offsetLeft
+      };
+
+  var action = false
+  canv.onmousedown = function(e) {
+      canv.style.cursor = "grab"
+      var p = Math.floor(X(e)/ clwidth)
+      action = p
+      };
+
+  canv.onmouseup = function(e) {
+      if(action || action == 0) {
+          canv.style.cursor = "default"
+          var p = Math.floor(X(e) / clwidth)
+          swap(action, p)
+          }
+      };
+
+  canv.addEventListener("touchstart", function(e) {
+      canv.onmousedown(e.touches[0])
+      })
+  
+  canv.addEventListener("touchend", function(e) {
+      canv.onmouseup(e.changedTouches[0])
+      })
+  
+  document.addEventListener("touchend", function() {
+      action = false
+      })
+  
+  document.onmouseup = function() {
+      canv.style.cursor = "default"
+      action = false
+      };
+
+  function resize() {
+      canv.style.marginLeft = (innerWidth-canv.width)/2 + "px"
+      canv.style.marginTop = (innerHeight-canv.height)/2 - 2*clwidth + "px"   
+      };
+
+  window.onresize = function() { resize() };
+
+  window.onload = function() {   entergame(); resize() };
+
---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+# Arrayality intro
+    A logical game JavaScript powered & light-weight
+
+# Gameplay
+   Swap the numbers in the array to get the highest score in the fewest moves. <br>
+   The score is calculated by multiplying a series of the same numbers. <br>
+   Units shall be next to each other, deuces through one, three through two, and so on.
+   
+# Running offline
+     
+    ```shell
+    # locate the directory
+    $ wget -r -np -nH https://vsnits.github.io/Arrayality/
+    # open Arrayality/index.html
+    ```
+
+ # Lisence
+   The Unlicense
+   
